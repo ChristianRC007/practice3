371420ee45e12fbc490890c2617650a44086378f
Andrey Cherniak <baza904@gmail.com>
2020-11-23 18:51:17 +0200

    Add slider buttons

--- js/script.js
+++ js/script.js
@@ -4,5 +4,7 @@ $(document).ready(function(){
   dots: true,
   slidesToShow: 3,
     slidesToScroll: 1,
+    prevArrow: '<button class="slider__button--left" aria-label="Previous" type="button"><svg><use href="./images/sprite.svg#icon-arrow-left"></use></svg></button>',
+    nextArrow: '<button class="slider__button--right" aria-label="Next" type="button"><svg><use href="./images/sprite.svg#icon-arrow-right"></use></svg></button>'
   });
 });
